9c743056b686091c81033207f07a895c2164b273
Håvard Aasmo <havi95@gmail.com>
2020-03-17 20:14:57 +0100

    Add plugin config file

--- src/main/java/net/riddlebit/mc/RiddleFactions.java
+++ src/main/java/net/riddlebit/mc/RiddleFactions.java
@@ -6,10 +6,12 @@ import net.riddlebit.mc.controller.FactionController;
 import net.riddlebit.mc.controller.PlayerController;
 import net.riddlebit.mc.controller.TreasureController;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RiddleFactions extends JavaPlugin {
 
+    public FileConfiguration config;
     public DataManager dataManager;
     public PlayerController playerController;
     public FactionController factionController;
@@ -17,6 +19,10 @@ public class RiddleFactions extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        config = getConfig();
+        config.options().copyDefaults(true);
+        saveConfig();
 
         dataManager = new DataManager(this);
 
